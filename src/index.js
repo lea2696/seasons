5582ccdd7e02cd553ebba5c73b47dd097e025246
Luis Alvarez <lea2696@gmail.com>
2019-07-26 11:12:51 -0300

    Render Condicional

--- src/index.js
+++ src/index.js
@@ -6,16 +6,26 @@ import ReactDOM from "react-dom"
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state={lat: null};
+        this.state={lat: null, error: null};
         window.navigator.geolocation.getCurrentPosition(
             position=>{this.setState({lat: position.coords.latitude})},
-        (err)=> console.log(err));
+        (err)=> this.setState({error: err.message}));
     }
     render(){
-         
-        return <div>Latitude: {this.state.lat} </div>
+        if(this.state.error && !this.state.lat){
+            return  <div> Error: {this.state.error}</div>     
+        }
+         if(!this.state.error && this.state.lat){
+             return <div>Latitude: {this.state.lat} </div>
+             
+         }
+       return (
+           <span>Loading</span>
+       );
     }
 }
+
+
 ReactDOM.render(
     <App/>, document.querySelector("#root")
 )
