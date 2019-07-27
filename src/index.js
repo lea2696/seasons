3191782ca7ac43449fffa47d165b3d27132d0c7c
Luis Alvarez <lea2696@gmail.com>
2019-07-27 10:13:21 -0300

    Se introdujo el loading y su valor por defecto

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loading from "./Loading"
 
 
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={lat: null, error: null};
+    state={lat: null, error: null};
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position=>{this.setState({lat: position.coords.latitude})},
+            position=>this.setState({lat: position.coords.latitude}), 
         (err)=> this.setState({error: err.message}));
+
     }
-    render(){
+    renderCondicional(){
         if(this.state.error && !this.state.lat){
             return  <div> Error: {this.state.error}</div>     
         }
          if(!this.state.error && this.state.lat){
-             return <div>Latitude: {this.state.lat} </div>
+             return <SeasonDisplay lat={this.state.lat} />
              
          }
        return (
-           <span>Loading</span>
+           <Loading message="Por favor acepta la ubicacion" />
        );
+    }
+    render(){
+        return(
+      <div>
+          {this.renderCondicional()}
+      </div>
+
+        )
     }
 }
 
